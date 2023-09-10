eb7221a8cda6f20a65a3fb6e2bb2b2b1ff3ff06f
Guts <ayhan-bey90@hotmail.com>
2023-09-10 13:08:25 +0300

    10eylül

--- Visualization/countryscript.js
+++ Visualization/countryscript.js
@@ -1,16 +1,45 @@
-fetch('https://restcountries.com/v3/all')
-.then(response => response.json())
+fetch("https://restcountries.com/v3/all").then(response => response.json())
 .then(data =>{
     const dataArray = Object.values(data);
 
-    const sortedData = dataArray.sort((a,b)=> b.population - a.population).slice(0,15)
-    const countriesList = document.getElementById("top-countries");
-    sortedData.forEach(country=>{
-        const listItem = document.createElement("li");
-        listItem.textContent=`${country.name.common} - nüfus: ${country.population}`;
+    const sortedData = dataArray.sort((a,b)=> b.population - a.population).slice(0,10);
+    const countriesList = document.getElementById('top-countries');
+    sortedData.forEach(country =>{
+        const listItem = document.createElement('li');
+        listItem.textContent=`${country.name.common}`;
         countriesList.appendChild(listItem);
-    });
-
-}).catch(error=>{
-    console.error("başarısız",error);
+    })
 });
+
+
+fetch('https://restcountries.com/v3/all')
+.then(response => response.json())
+  .then(data => {
+    // API'den gelen veriyi bir diziye çıkarma
+    const countriesArray = Object.values(data);
+
+    // Tüm dilleri toplama
+    const allLanguages = [];
+    countriesArray.forEach(country => {
+      if (country.languages) {
+        const countryLanguages = Object.keys(country.languages);
+        allLanguages.push(...countryLanguages);
+      }
+    });
+    const languageCountys ={};
+    allLanguages.forEach(language=>{
+        if(languageCountys[language]){
+            languageCountys[language]++;
+        }
+        else {
+            languageCountys[language]=1;
+        }
+    });
+    const sortedLanguages = Object.keys(languageCountys).sort((a,b)=> languageCountys[b] - languageCountys[a]).slice(0,15);
+    const languagesList = document.getElementById('top-languages');
+    sortedLanguages.forEach(language=>{
+        const listItem = document.createElement('li');
+        listItem.textContent= `${languageCountys[language]}`;
+        languagesList.appendChild(listItem);
+    })
+})
